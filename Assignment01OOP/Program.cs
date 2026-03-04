e7f468c3741bd464ddd9e5b99f6947dfcd660368
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-03-05 00:27:56 +0200

    Question01

--- Assignment01OOP/Program.cs
+++ Assignment01OOP/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+
+            /* Class : 
+             *      - Reference Type 
+             *      - stored in heap with a reference in stack
+             *      - inheritance is supported
+             *      - best for complex content
+             *      - parameterless constructors are made by compiler unless you made a parameter constructor
+               Struct : 
+                    - Value Type
+                    - Stored in stack
+                    - inheritance is not supported
+                    - parameterless constructor is always made by compiler
+                    - better when working with small data
+            */
+            Person p;
+            //p = new();
+            //p.Name = "ahmed";
+            //Person p2 = new Person();
+            //p2 = p;
+            //p2.Name = "Youssef";
+            //Console.WriteLine(p.Name);//ahmed
+
+            //Student person1 = new Student();
+            //person1.Name = "Ali";
+            //Student person2 = new Student();
+            //person2 = person1;
+            //person2.Name = "Mohamed";
+            //Console.WriteLine(person1.Name);//Mohamed
+
+            #endregion
         }
     }
 }
